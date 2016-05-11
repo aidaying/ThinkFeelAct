26ad3074544bf1d40e1692c97259a6bde24aee4d
Aida <aida_ying@hotmail.com>
2016-05-11 16:57:24 +1200

    Created Long Term Survey Activity

---
+++ app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/LongTermSurveyActivityFragment.java
@@ -0,0 +1,22 @@
+package nz.ac.aut.rnd.team.thinkfeelactproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LongTermSurveyActivityFragment extends Fragment {
+
+    public LongTermSurveyActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_long_term_survey, container, false);
+    }
+}
