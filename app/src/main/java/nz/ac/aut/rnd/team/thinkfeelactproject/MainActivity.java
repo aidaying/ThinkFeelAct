47f43ad4d16c4f42bdd6dcaab9e97a4562e234a7
Aida <aida_ying@hotmail.com>
2016-05-17 23:49:46 +1200

    Second Commit
    Initiate Database, set up all the table, all the question, set up the
    fragment transaction

--- app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/MainActivity.java
+++ app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/MainActivity.java
@@ -1,13 +1,40 @@
 package nz.ac.aut.rnd.team.thinkfeelactproject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int fragment_first_time_initial_page=1;
+    private static final int fragment_first_time_survey=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch(getIntent().getIntExtra("fragment_id",1)){
+            case fragment_first_time_initial_page:
+                FirstTimeInitialPageFragment firstTimeInitialPageFragment = new FirstTimeInitialPageFragment();
+                fragmentTransaction.add(R.id.fragment_container, firstTimeInitialPageFragment);
+                break;
+
+            case fragment_first_time_survey:
+                FirstTimeSurveyFragment firstTimeSurveyFragment = new FirstTimeSurveyFragment();
+
+                fragmentTransaction.add(R.id.fragment_container, firstTimeSurveyFragment);
+                break;
+
+        }
+        fragmentTransaction.commit();
     }
+
 }
