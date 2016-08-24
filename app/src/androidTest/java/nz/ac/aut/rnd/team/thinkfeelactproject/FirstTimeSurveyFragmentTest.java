6447ae353462d7d4bb388fe8ffbb06d203e5f67c
wangzihang008 <wangzihang008@gmail.com>
2016-08-25 00:00:21 +1200

    Added unit tests of Activities
    1. Added unit test of MainAcivity
    2. Added unit test of FirstTimeSurveyFragment

---
+++ app/src/androidTest/java/nz/ac/aut/rnd/team/thinkfeelactproject/FirstTimeSurveyFragmentTest.java
@@ -0,0 +1,29 @@
+package nz.ac.aut.rnd.team.thinkfeelactproject;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Robert on 9/08/2016.
+ */
+public class FirstTimeSurveyFragmentTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOnCreateView() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOnClick() throws Exception {
+
+    }
+}
---
+++ app/src/androidTest/java/nz/ac/aut/rnd/team/thinkfeelactproject/MainActivityTest.java
@@ -0,0 +1,38 @@
+package nz.ac.aut.rnd.team.thinkfeelactproject;
+
+import android.support.test.*;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.test.AndroidTestCase;
+import junit.framework.Assert;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+
+/**
+ * Created by Robert on 9/08/2016.
+ */
+
+@LargeTest
+public class MainActivityTest extends AndroidTestCase {
+
+    private MainActivity mainActivity;
+    private Button button;
+
+    @Override
+    public void setUp() throws Exception {
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+
+    }
+
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
