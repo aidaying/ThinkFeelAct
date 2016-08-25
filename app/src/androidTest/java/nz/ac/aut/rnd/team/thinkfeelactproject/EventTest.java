a53d2c62875d2024a77b93de4081797497a279b7
wangzihang008 <wangzihang008@gmail.com>
2016-08-25 14:03:17 +1200

    Updated unit tests
    1. Updated unit test of Event class
    2. Updated unit test of StressCalculator class
    3. Updated unit test of survey class

---
+++ app/src/androidTest/java/nz/ac/aut/rnd/team/thinkfeelactproject/EventTest.java
@@ -0,0 +1,78 @@
+package nz.ac.aut.rnd.team.thinkfeelactproject;
+
+import org.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.*;
+import junit.framework.Assert;
+
+import static junit.framework.Assert.assertEquals;
+
+
+/**
+ * Created by Robert on 9/08/2016.
+ */
+public class EventTest {
+
+    private Event event;
+
+    @Before
+    public void setUp() throws Exception {
+        event = new Event();
+        event.setDate("111");
+        event.setID(1);
+        event.setName("aaa");
+        event.setRating(1.0);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        event = null;
+    }
+
+    @Test
+    public void testGetID() throws Exception {
+        assertEquals(1, event.getID());
+    }
+
+    @Test
+    public void testSetID() throws Exception {
+        event.setID(2);
+        assertEquals(2, event.getID());
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        assertEquals("aaa", event.getName());
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+        event.setName("bbb");
+        assertEquals("bbb", event.getName());
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+        assertEquals("111", event.getDate());
+    }
+
+    @Test
+    public void testSetDate() throws Exception {
+        event.setDate("222");
+        assertEquals("222", event.getDate());
+    }
+
+    @Test
+    public void testGetRating() throws Exception {
+        assertEquals(1.0, event.getRating());
+    }
+
+    @Test
+    public void testSetRating() throws Exception {
+        event.setRating(2.0);
+        assertEquals(2.0, event.getRating());
+    }
+}
