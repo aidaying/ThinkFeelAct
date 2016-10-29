e9d87b9697f3a3f07e74c7b1d3c8c665faebe626
Aida <aida_ying@hotmail.com>
2016-10-29 18:30:07 +1300

    Minor Layout Update
    Fixed the long term result list display issue in Time Line model

--- app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/sosmodel/SOSModelActivity.java
+++ app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/sosmodel/SOSModelActivity.java
@@ -40,7 +40,16 @@ import nz.ac.aut.rnd.team.thinkfeelactproject.R;
 import nz.ac.aut.rnd.team.thinkfeelactproject.java.DatabaseHandler;
 import nz.ac.aut.rnd.team.thinkfeelactproject.java.Desc;
 
-
+/**
+ * SOS model is the Same old Stuff keep happening model
+ * This model has implemented a pie chart where user can see the Phase of the cycle
+ * that will happen to the person after they have repeated the event that might be violence, or have suicide thoughts, etc.
+ * ////////////////////////////////////Implemented//////////////////////////////////////////
+ * XML parser which gets the data from the /raw/sospie.xml to generate the pie chart details.
+ * Pie chart that is using MPChart open source plugin found in GitHub
+ * Linked to Database to store all the event that is inputted by users
+ * Implemented dialogs to show the view and the other one for adding the data.
+ */
 public class SOSModelActivity extends Activity {
 
     private DatabaseHandler mydb;
@@ -49,13 +58,14 @@ public class SOSModelActivity extends Activity {
     private float[]  yData;
     private String[] xData;
     private sosDescListAdapter sosDescListAdapter;
-    private Button addSoSListBtn;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sosmodel);
         mydb = new DatabaseHandler(getApplicationContext());
+
         XmlPullParserFactory pullParserFactory;
         try{
             pullParserFactory = XmlPullParserFactory.newInstance();
@@ -67,9 +77,7 @@ public class SOSModelActivity extends Activity {
         }catch(XmlPullParserException | IOException e){
             e.printStackTrace();
         }
-
-
-
+        //////////////////pie Chart Implementation start/////////////////////
         relativeLayout = (RelativeLayout) findViewById(R.id.sospie);
         sosSelection = new PieChart(this);
 
@@ -79,22 +87,22 @@ public class SOSModelActivity extends Activity {
         sosSelection.setDescription("");
         sosSelection.setDrawHoleEnabled(true);
         sosSelection.setHoleColor(Color.TRANSPARENT);
-        sosSelection.setHoleRadius(60);
-        sosSelection.setTransparentCircleRadius(60);
+        sosSelection.setHoleRadius(30);
+        sosSelection.setTransparentCircleRadius(30);
 
         sosSelection.setRotationAngle(0);
         sosSelection.setRotationEnabled(false);
-        addSoSListBtn = (Button) relativeLayout.findViewById(R.id.addSOSListBtn);
-
 
 
         ViewGroup.LayoutParams params = sosSelection.getLayoutParams();
         params.height = ViewGroup.LayoutParams.MATCH_PARENT;
         params.width = ViewGroup.LayoutParams.MATCH_PARENT;
 
-        addSoSListBtn.setOnClickListener(new View.OnClickListener() {
+
+        sosSelection.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public void onValueSelected(Entry e, Highlight h) {
+                //////////////////First Dialog///////////////
                 final Dialog dialog = new Dialog(SOSModelActivity.this);
                 dialog.setContentView(R.layout.fragment_sos_dialog);
                 dialog.setTitle("Details");
@@ -105,6 +113,7 @@ public class SOSModelActivity extends Activity {
                 addNewBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        //////////////////Second Dialog/////////////////////////////////////
                         final Dialog addNewDialog = new Dialog(SOSModelActivity.this);
                         addNewDialog.setContentView(R.layout.addnewdesclayout);
                         addNewDialog.setTitle("Add New");
@@ -116,35 +125,33 @@ public class SOSModelActivity extends Activity {
                         addNewDesc.addTextChangedListener(new TextWatcher() {
                             @Override
                             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
                             }
 
                             @Override
                             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                                    addNewDescBtn.setEnabled(s.toString().trim().length() > 0);
+                                addNewDescBtn.setEnabled(s.toString().trim().length() > 0);
                             }
 
                             @Override
                             public void afterTextChanged(Editable s) {
-
                             }
                         });
-                            addNewDescBtn.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    String type = "SOS";
-                                    String desc = addNewDesc.getText().toString();
-                                    int num = 1;
-                                    Desc desc1 = new Desc();
-                                    desc1.setDesc(desc);
-                                    desc1.setType(type);
+                        addNewDescBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String type = "SOS";
+                                String desc = addNewDesc.getText().toString();
+                                int num = 1;
+                                Desc desc1 = new Desc();
+                                desc1.setDesc(desc);
+                                desc1.setType(type);
 
-                                    desc1.setNum(num);
-                                    mydb.addNewDescInfo(desc1);
-                                    updateList(dialog);
-                                    addNewDialog.dismiss();
-                                }
-                            });
+                                desc1.setNum(num);
+                                mydb.addNewDescInfo(desc1);
+                                updateList(dialog);
+                                addNewDialog.dismiss();
+                            }
+                        });
 
                         cancelAddNewDescBtn.setOnClickListener(new View.OnClickListener() {
                             @Override
@@ -162,20 +169,31 @@ public class SOSModelActivity extends Activity {
                     }
                 });
             }
-        });
-        addData();
 
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        addData();
+        //////////////////pie Chart Implementation end/////////////////////
     }
 
 
-
+    /**
+     * This method is linked to the sosDescListAdaptor to update the list
+     * sosDescListAdaptor is a class to set up each row of the data in the list
+     *
+     * @param dialog
+     */
     private void updateList(Dialog dialog){
         if(mydb.isDescEmpty() == true) {
             ArrayList<Desc> descList = mydb.getAllNewDescInfo();
             ArrayList<Desc> list = new ArrayList<Desc>();
             for(int i = 0; i < descList.size(); i++){
 
-                        list.add(descList.get(i));
+                list.add(descList.get(i));
 
             }
             sosDescListAdapter = new sosDescListAdapter(dialog.getContext(), list);
@@ -186,6 +204,9 @@ public class SOSModelActivity extends Activity {
         }
     }
 
+    /**
+     * This method is used to set up the pie chart data and colour of each portion of the chart
+     */
     private void addData(){
 
         ArrayList<PieEntry> yVals1 = new ArrayList<PieEntry>();
@@ -205,15 +226,7 @@ public class SOSModelActivity extends Activity {
         for(int c : ColorTemplate.JOYFUL_COLORS){
             colors.add(c);
         }
-        for(int c : ColorTemplate.JOYFUL_COLORS){
-            colors.add(c);
-        }
-        for(int c : ColorTemplate.JOYFUL_COLORS){
-            colors.add(c);
-        }
-        for(int c : ColorTemplate.JOYFUL_COLORS){
-            colors.add(c);
-        }
+
         colors.add(ColorTemplate.getHoloBlue());
         pieDataSet.setColors(colors);
 
@@ -225,6 +238,8 @@ public class SOSModelActivity extends Activity {
         sosSelection.highlightValue(null);
         sosSelection.invalidate();
     }
+
+
     private void parserXML(XmlPullParser parser) throws XmlPullParserException, IOException{
         int i = 0;
         int j = 0;
