b6d149a6f70a66d33d1e53ae607e644891de26ab
Aida <aida_ying@hotmail.com>
2016-10-30 22:06:08 +1300

    BackPress Issue Fixed
    Fixed the backpressed issue
    Fixed the layout

--- app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/firsttimeloadup/FirstTimeLauncherSurveysActivity.java
+++ app/src/main/java/nz/ac/aut/rnd/team/thinkfeelactproject/firsttimeloadup/FirstTimeLauncherSurveysActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import nz.ac.aut.rnd.team.thinkfeelactproject.R;
 
@@ -11,7 +12,8 @@ public class FirstTimeLauncherSurveysActivity extends FragmentActivity {
 
     private static final int LTsurvey = 1;
     private static final int STevent = 2;
-
+    FirstTimeSurveyFragment firstTimeSurveyFragment;
+    AddEventPageFragment addEventPageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,15 @@ public class FirstTimeLauncherSurveysActivity extends FragmentActivity {
 
         switch(getIntent().getIntExtra("fragment_id",1)){
             case LTsurvey:
-                FirstTimeSurveyFragment firstTimeSurveyFragment = new FirstTimeSurveyFragment();
+                firstTimeSurveyFragment = new FirstTimeSurveyFragment();
                 fragmentTransaction.add(R.id.fragment_container, firstTimeSurveyFragment);
                 break;
             case STevent:
-                AddEventPageFragment addEventPageFragment = new AddEventPageFragment();
+                addEventPageFragment = new AddEventPageFragment();
                 fragmentTransaction.add(R.id.fragment_container, addEventPageFragment);
                 break;
         }
         fragmentTransaction.commit();
     }
+
 }
